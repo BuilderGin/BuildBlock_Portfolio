a3e3d73517594ce5a5428c30333341892cea6dae
Gin <ginggeli@hotmail.com>
2021-04-28 00:02:19 +0200

    add allowed Host

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '%g0fz8yedo*3)l4yz@(yu%@497!da+)q_vlr$w#l4d)i@-b=-b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'portfolio-buildblock.herokuapp.com']
 
 
 # Application definition
